0848cd155b73d5f8d68f0044f3005604c8fe65d7
Punsano26 <xlichaphlreuxn.gmail.com>
2023-02-23 16:46:12 +0700

    Update 16:45 23/02/2566

--- index.js
+++ index.js
@@ -32,3 +32,31 @@ var swiper = new Swiper (".home-slider",{
     },
     loop:true
 });
+
+var swiper = new Swiper (".review-slider",{
+    spaceBetween:30,
+    centeredSlides:true,
+    autoplay:{
+        delay:3000,//เวลาในการเลื่อนสไลด์ (millisecond)
+        disableOnInteraction:false,
+    },
+    pagination :{
+        el:".swiper-pagination",
+        clickable:true,
+    },
+    loop:true,
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        640:{
+            slidesPerView:2,
+        },
+        768:{
+            slidesPerView:2,
+        },
+        1024:{
+            slidesPerView:3,
+        }
+    }
+});
